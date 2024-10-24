5d078bc6a26c0088133c7f29da46745f4fba2434
Antonio <tonofalcon22@gmail.com>
2024-10-24 16:47:47 -0600

    test-3 image netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
 
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
